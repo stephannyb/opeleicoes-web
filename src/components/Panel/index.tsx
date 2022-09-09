2a39e1edc7205bb3dd795fa766bdc0f726dacc7f
balbboa <abmm_arthur@hotmail.com>
2022-09-09 12:47:20 -0300

    Feat: modal for each card dev

--- src/components/Panel/index.tsx
+++ src/components/Panel/index.tsx
@@ -1,105 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import Modal from 'react-modal';
-import api from '../../services/api';
-import Button from '../Button';
-import {
-  CardContent,
-  CardPanel,
-  ModalContent,
-  TextCard,
-  TittleModal
-} from './styles';
-
-interface Items {
-  cidade: string;
-  comando: string;
-  descricao: string;
-  desfecho: string;
-  id: string;
-  ocorrencia: string;
-  operacao: string;
-  opm: string;
-  st_matricula: string;
-  st_nomeguerra: string;
-  st_postograduacao: string;
-  tipo: string;
-  uf: string;
-  zona: string;
-}
-
-type ItemsArray = Array<Items>;
+import React from 'react';
+import { CardContent, CardPanel, TextCard } from './styles';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export default function Panel() {
-  const [open, setOpen] = useState(false);
-  const [card, setCard] = useState<Items[]>([]);
-
-  const customStyles = {
-    content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
-    },
-  };
-
-  const loadUser = async () => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    await api
-      .post('validar', {
-        cidade: 'Natal',
-      })
-      .then(res => {
-        setCard(res.data.res);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
-
-  useEffect(() => {
-    // eslint-disable-next-line no-use-before-define
-    loadUser();
-  }, []);
-
-  function openModal() {
-    setOpen(true);
-  }
-
-  function closeModal() {
-    setOpen(false);
-  }
-
+export default function Panel({
+  show,
+  nome,
+  contato,
+  cidade,
+  ocorrencia,
+}: any) {
   return (
     <>
-      <CardPanel onClick={openModal}>
+      <CardPanel onClick={show}>
         <CardContent>
           <>
-            <TextCard>{card[0]?.st_nomeguerra}</TextCard>
-            <TextCard>Contato:</TextCard>
-            <TextCard>{card[0]?.cidade}</TextCard>
-            <TextCard>{card[0]?.ocorrencia}</TextCard>
+            <TextCard>{nome}</TextCard>
+            <TextCard>{contato}</TextCard>
+            <TextCard>{cidade}</TextCard>
+            <TextCard>{ocorrencia}</TextCard>
           </>
         </CardContent>
       </CardPanel>
-
-      <Modal isOpen={open} style={customStyles}>
-        <TittleModal>
-          <TextCard className="modal">{card[0]?.st_nomeguerra}</TextCard>
-          <Button className="close" onClick={closeModal}>
-            X
-          </Button>
-        </TittleModal>
-        <TextCard className="modal">{card[0]?.ocorrencia}</TextCard>
-        <TextCard className="modal">{card[0]?.descricao}</TextCard>
-        <TextCard className="modal">{card[0]?.desfecho}</TextCard>
-        <ModalContent>
-          <Button>Validar</Button>
-          <Button className="secondary">Invalidar</Button>
-        </ModalContent>
-      </Modal>
     </>
   );
 }
